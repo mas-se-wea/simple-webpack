e338bd25b4783ea0eac7cd9ed3ade5f2ca46a9ba
Tobias Büchel <tobi.buechel@gmail.com>
2020-08-24 12:42:59 +0200

    Add webpack dev server & production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,14 +1,10 @@
 const path = require('path')
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
   }
 }
